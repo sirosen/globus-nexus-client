967f93cfc60088eb55d6661c0d2da82087d017a5
Stephen Rosen <sirosen@uchicago.edu>
2016-10-26 10:52:55 -0500

    Update to SDK 0.4.3 compatibility

--- globus_nexus_client/client.py
+++ globus_nexus_client/client.py
@@ -1,5 +1,6 @@
-from globus_sdk import config
-from globus_sdk.exc import GlobusError
+from globus_sdk import (
+    AccessTokenAuthorizer, RefreshTokenAuthorizer, BasicAuthorizer,
+    exc)
 from globus_sdk.base import BaseClient
 
 
@@ -19,39 +20,26 @@ class NexusClient(BaseClient):
     ``nexus_token`` (under the ``[general]`` heading, like ``auth_token`` and
     ``transfer_token``).
     """
-    AUTHTYPE_GOAUTH_TOKEN = "goauth_token"
+    allowed_authorizer_types = [AccessTokenAuthorizer, RefreshTokenAuthorizer,
+                                BasicAuthorizer]
 
-    def __init__(self, environment=config.get_default_environ(), token=None,
-                 app_name=None):
-        BaseClient.__init__(self, "nexus", environment, token=token,
-                            app_name=app_name)
+    def __init__(self, **kwargs):
+        BaseClient.__init__(self, "nexus", **kwargs)
 
-    def config_load_token(self):
-        return config._get_token('nexus_token', self.environment)
-
-    def set_token(self, token):
-        """
-        Overrides default method, which would set a token as a Bearer token.
-        We need to set it to the custom Globus-Goauthtoken Authentication type.
-        """
-        self.auth_type = self.AUTHTYPE_GOAUTH_TOKEN
-        self._headers['Authorization'] = 'Globus-Goauthtoken {}'.format(token)
-
-    def get_goauth_token(self, username, password):
+    def get_goauth_token(self):
         """
         Note that these tokens have a long lifetime and should be
         saved and re-used.
 
         :rtype: string
         """
-        self.set_auth_basic(username, password)
+        if not isinstance(self.authorizer, BasicAuthorizer):
+            raise exc.GlobusError('get_goauth_token() requires basic auth')
         r = self.get('/goauth/token?grant_type=client_credentials')
         try:
             return r['access_token']
         except KeyError:
-            raise GlobusError(r)
-        else:
-            raise
+            raise exc.GlobusAPIError(r)
 
     def get_user(self, username):
         """
