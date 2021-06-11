16e1fd6214fae9b1ed1aa805e4a0bfaef1214a0b
Stephen Rosen <sirosen@globus.org>
2021-06-11 16:10:42 +0000

    Move all package metadata into setup.cfg

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="globus-nexus-client",
-    version="0.4.0a1",
-    description="Unofficial Globus Nexus Client (based on SDK clients)",
-    long_description=open("README.rst").read(),
-    author="Stephen Rosen",
-    author_email="support@globus.org",
-    url="https://github.com/sirosen/globus-nexus-client",
-    py_modules=["globus_nexus_client"],
-    install_requires=["globus-sdk==3.0.0a2"],
-    keywords=["globus", "groups", "nexus"],
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-)
+setup()
